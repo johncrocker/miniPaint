4d0e2dbd61a41809fb81ee47838526dc2cdf49ff
John Crocker <crockerish@gmail.com>
2024-07-12 15:01:58 +0100

    Adde mutiarch capable

--- buildutils/publish.sh
+++ buildutils/publish.sh
@@ -9,8 +9,15 @@ cd $HOME/src/miniPaint
 
 docker buildx create --name multiarchbuilder --use --bootstrap
 
-docker buildx build --push . \
-        --platform linux/arm64 \
-        -t "crockerish/minipaint:$version"
+if [ "$version" = "latest" ]; then
+        docker buildx build --push . \
+		--platform linux/arm64 \
+                -t "crockerish/minipaint:$version" \
+		-t "crockerish/minipaint:4.14.2"
+else
+	docker buildx build --push . \
+		--platform linux/arm64 \
+  	        -t "crockerish/minipaint:$version"
+fi
 
 docker buildx rm multiarchbuilder
